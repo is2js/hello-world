4f2483d484a62c980534706b496271a79c2fdf1c
is2js <tingstyle1@gmail.com>
2021-11-12 11:14:21 +0900

    hi world 메세지 추가

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello javaJigi");
 		System.out.println("Hi world");
+		System.out.println("변경된 사항입니다.");
 	}
 }
